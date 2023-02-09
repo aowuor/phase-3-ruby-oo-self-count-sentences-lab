911ee896d1433500b697c315de210e2eaad1832b
aowuor <angelaowuor@gmail.com>
2023-02-10 00:32:35 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
-require 'pry'
+# require 'pry'
 
 class String
+  attr_accessor :str
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
 
+    self.split(/(?<=[.!?]).(?=[a-zA-Z])/).count
   end
+
+  # binding.pry
 end
+
+
